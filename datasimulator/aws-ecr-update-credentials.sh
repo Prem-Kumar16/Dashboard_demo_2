264f2dac556fe7fb00b7653244f0b9b1881e54e6
Prem Kumar S <75419846+Prem-Kumar16@users.noreply.github.com>
2023-07-27 23:11:26 +0530

    Create aws-ecr-update-credentials.sh

---
+++ datasimulator/aws-ecr-update-credentials.sh
@@ -0,0 +1,12 @@
+sudo /usr/local/bin/kubectl delete pod datasimulatordemo
+sudo /usr/local/bin/kubectl delete service datasimulator-app-service
+aws_access_key_id=$(aws --profile default configure get aws_access_key_id)
+aws_secret_access_key=$(aws --profile default configure get aws_secret_access_key)
+aws configure set aws_access_key_id "$aws_access_key_id"
+aws configure set aws_secret_access_key "$aws_secret_access_key"
+sudo /usr/local/bin/kubectl delete secret regcred
+sudo /usr/local/bin/kubectl create secret docker-registry regcred \
+  --docker-server=783584839454.dkr.ecr.ap-south-1.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password=$(aws ecr get-login-password --region ap-south-1)
+sudo /usr/local/bin/kubectl apply -f /home/ewaol/Dashboard_demo_2/datasimulator/deployaws.yaml -f /home/ewaol/Dashboard_demo_2/datasimulator/service.yaml
